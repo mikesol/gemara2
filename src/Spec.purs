06d63317a47af72de295fc364793a60ade91d92e
Mike Solomon <mike@meeshkan.com>
2020-03-20 14:20:58 +0200

    Scaffolds ps

--- src/Spec.purs
+++ src/Spec.purs
@@ -1,8 +1,27 @@
 module Spec where
 import Prelude
+import Data.Maybe (Maybe)
+import Data.List (List)
+import Effect (Effect)
 import Effect.Console (log)
+
+data PathPiece = PString String | PInt Int
+
+type RequestHeaders =
+  { authorization :: Maybe String
+  , x_next :: Maybe String
+  , x_signature :: Maybe String
+  }
+
+type Request =
+  { path :: List PathPiece
+  , headers  :: RequestHeaders
+  }
 
 greet :: String -> String
 greet name = "Hello, " <> name <> "!"
 
+main :: Effect Unit
 main = log (greet "World")
+
+-- spec :: Request -> Response
